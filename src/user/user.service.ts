cc197f358cc1e7fc0ba6e292030fa763546efa38
sum bùi <sumbn37k02.1@gmail.com>
2024-07-14 04:56:35 +0700

    search

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { DeleteResult, Like, Repository, UpdateResult } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -17,8 +17,13 @@ export class UserService {
     const items_per_page = Number(query.items_per_page) || 10;
     const page = Number(query.page) || 1;
     const skip = (page - 1) * items_per_page;
-
+    const keyword = query.search || '';
     const [result, total] = await this.userRepository.findAndCount({
+      where: [
+        { first_name: Like('%' + keyword + '%') },
+        { last_name: Like('%' + keyword + '%') },
+        { email: Like('%' + keyword + '%') },
+      ],
       order: { created_at: 'DESC' },
       take: items_per_page,
       skip: skip,
